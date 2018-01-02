ff30e7eecf4b3ad8ea8ffb759c3cd70588a53e67
D'Mita Levy <dlevy022@fiu.edu>
2018-01-02 02:14:35 -0500

    added angular timepicker. Testing mdbootstrap again

--- src/main/resources/static/js/jobs/post/new-job-post-form-2.js
+++ src/main/resources/static/js/jobs/post/new-job-post-form-2.js
@@ -2,7 +2,7 @@
  * 
  */
 
-var app = angular.module('jobPostApp', []);
+var app = angular.module('jobPostApp', ['moment-picker']);
 
 app.controller('jobPostCtrl',['$scope','$http', function($scope,$http) {
 		
